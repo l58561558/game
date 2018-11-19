469430cf760dd817c230df96b313c6490d9f5ad8
adminzz <58561558@qq.com>
2018-11-19 15:58:03 +0800

    remove

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '10.72.164.49',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'game',
     // 用户名
